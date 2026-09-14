99d0bd662ed1259e38d13a87004064ac6f8ad474
SHREE <shreebalajihospital767@gmail.com>
2026-09-14 13:06:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def index():
+def home():
     return render_template("index.html")
 
 
@@ -14,8 +14,4 @@ def health():
 
 
 if __name__ == "__main__":
-    app.run(
-        host="0.0.0.0",
-        port=5000,
-        debug=True
-    )
+    app.run(host="0.0.0.0", port=5000, debug=True)
